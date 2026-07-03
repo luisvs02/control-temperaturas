92eb7d6ec5003f104312878884ed988b129678d6
luisvs02 <luisvenialgo02s@gmail.com>
2026-07-03 17:13:40 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,8 @@ const ASSETS = [
   "./index.html",
   "./manifest.json",
   "./icon-192.png",
-  "./icon-512.png"
+  "./icon-512.png",
+  "./logo-vitapan.png"
 ];
 
 self.addEventListener("install", (event) => {
